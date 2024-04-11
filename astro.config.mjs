f7a8a205211f623a6db6846e4459500604ecc371
CharlotteAce <149445926+CharlotteAce@users.noreply.github.com>
2024-04-11 20:43:52 +0900

    astro.configの修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
-import mdx from "@astrojs/mdx";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://astronaut.github.io',
-  integrations: [mdx()]
+	site: 'https://blog.alexafazio.dev',
+	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex]
+	}
 });
